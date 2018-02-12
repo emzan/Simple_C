a9360364a132400a017c474eab004708f57a4cdd
emzan <zanettiemilio@hotmail.com>
2018-02-12 14:00:12 +0800

    Initial Commit

---
+++ arrayFunc/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  arrayFunc
+//
+//  Created by George Martinson on 12/02/2018.
+//  Copyright © 2018 Emilio Zanetti. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
