7371f408fd30cd457aa079e10362bb0a3df1f8fb
emzan <zanettiemilio@hotmail.com>
2018-02-21 22:53:29 +0800

    noRedefinitionAllowed

---
+++ Simple_C/variables_0.c
@@ -0,0 +1,18 @@
+//
+//  variables_0.c
+//  arrayFunc
+//
+//  Created by George Martinson on 21/02/2018.
+//  Copyright © 2018 Emilio Zanetti. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    double x = 3.4;
+    x = 9.7;
+    printf("%f\n",x);
+    
+    return 0;
+}
