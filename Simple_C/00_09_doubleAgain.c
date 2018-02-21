30c95f0c3ae81c302194f3b43e9fbf815b2e62eb
emzan <zanettiemilio@hotmail.com>
2018-02-21 23:43:39 +0800

    it's a double !

---
+++ Simple_C/00_09_doubleAgain.c
@@ -0,0 +1,17 @@
+//
+//  doubleAgain.c
+//  arrayFunc
+//
+//  Created by George Martinson on 21/02/2018.
+//  Copyright © 2018 Emilio Zanetti. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, char **argv){
+    double y = 5;  //here it recognizes 5 as a double!
+    double x = 9/y;
+    printf("%f\n",x);
+    
+    return 0;
+}
