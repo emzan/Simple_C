46afef68d8760ed11ba28eb4b8cb4836b036b0e4
emzan <zanettiemilio@hotmail.com>
2018-02-21 21:41:58 +0800

    typesFile

---
+++ Simple_C/types.c
@@ -0,0 +1,15 @@
+//
+//  types.c
+//  arrayFunc
+//
+//  Created by George Martinson on 21/02/2018.
+//  Copyright © 2018 Emilio Zanetti. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    printf("%c\n",97);
+    //return 0;
+}
+
