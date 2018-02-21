69cfca0b40c80768706ebfa1696f2bfb4a2c9596
emzan <zanettiemilio@hotmail.com>
2018-02-21 21:58:43 +0800

    characters

---
+++ Simple_C/types_0.c
@@ -0,0 +1,17 @@
+//
+//  types.c
+//  arrayFunc
+//
+//  Created by George Martinson on 21/02/2018.
+//  Copyright © 2018 Emilio Zanetti. All rights reserved.
+//
+
+#include <stdio.h>
+#include <sys/limits.h>
+#include <math.h>
+#include <float.h>
+
+int main(int argc, char **argv) {
+    printf("char:\n");
+    printf(" unsigned min,max: %d,%d (0,UCHAR_MAX)\n",SCHAR_MIN,SCHAR_MAX);
+}
