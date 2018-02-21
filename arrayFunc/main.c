376aea217d704b6d76506719b5f849a7c8611644
emzan <zanettiemilio@hotmail.com>
2018-02-21 20:25:50 +0800

    FirstFileAbsolutelyCommitted

--- arrayFunc/main.c
+++ arrayFunc/main.c
@@ -1,15 +1,9 @@
-//
-//  main.c
-//  arrayFunc
-//
-//  Created by George Martinson on 12/02/2018.
-//  Copyright © 2018 Emilio Zanetti. All rights reserved.
-//
-
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+
+int main(int argc, char **argv)
+{
+   
     return 0;
 }
+
