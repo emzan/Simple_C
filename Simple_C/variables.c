1123db48d1822d7d65c000e0040a14d435155e9b
emzan <zanettiemilio@hotmail.com>
2018-02-21 22:33:44 +0800

    goodLuck

---
+++ Simple_C/variables.c
@@ -0,0 +1,15 @@
+//
+//  variables.c
+//  arrayFunc
+//
+//  Created by George Martinson on 21/02/2018.
+//  Copyright © 2018. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Ciao\n");
+    return 0;
+}
